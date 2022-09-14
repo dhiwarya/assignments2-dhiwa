e1a6f0c1974fc87ce16126af9471c2be0b3bc4ef
Dhiwa Arya Kusumah <dhiwaarya@Dhiwas-MacBook-Pro.local>
2022-09-14 23:46:24 +0700

    Assignment2 is ready

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
 # TODO: Create your views here.
+from django.shortcuts import render
+from katalog.models import CatalogItem
+
+# Create your views here.
+
+def show_catalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_catalog_item,
+        'name': 'Dhiwa Arya Kusumah',
+        'npm' : '2106657115',
+    }
+    return render(request, "katalog.html", context)
