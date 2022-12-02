2745c96649d7fee8ded4c3d90e4903b3ff5b8024
Dhiwa Arya Kusumah <dhewarya@gmail.com>
2022-12-02 18:04:38 +0700

    fix models mywatchlist

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Mywatchlist(models.Model):
-    watched = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
+    watched = models.BooleanField()
+    title = models.TextField()
     rating = models.IntegerField()
-    release_date = models.CharField(max_length=255)
+    release_date = models.TextField()
     review = models.TextField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -7,12 +7,24 @@ from mywatchlist.models import Mywatchlist
 # Create your views here.
 def show_mywatchlist(request):
     data_mywatchlist_item = Mywatchlist.objects.all()
-    context = {
-        'list_item': data_mywatchlist_item,
+    nonton = 0
+    belom = 0
+
+    for i in data_mywatchlist_item:
+        if i.watched == True:
+            nonton += 1
+        else:
+            belom += 1
+            
+    context ={
+        'list_item' : data_mywatchlist_item,
         'name': 'Dhiwa Arya Kusumah',
-        'npm': '2106657115',
+        'NPM' : '2106657115',
+        'nonton' : nonton,
+        'belom' : belom,
     }
-    return render(request, "mywatchlist.html", context)    
+    
+    return render(request,"mywatchlist.html", context)   
 
 def show_xml(request):
     data = Mywatchlist.objects.all()
